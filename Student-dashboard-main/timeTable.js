98750f63ca1f1b0f302955887a126ef73b69c6d7
jeevithacit <132272030+jeevithacit@users.noreply.github.com>
2024-02-03 00:49:19 +0530

    Add files via upload

---
+++ Student-dashboard-main/timeTable.js
@@ -0,0 +1,133 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'F1',
+        subject: 'MA2401',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'F1',
+        subject: 'IT2401',
+        type: 'Lab Session'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'F1',
+        subject: 'IT2403',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'F1',
+        subject: 'IT2405',
+        type: 'Tutorial'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'F1',
+        subject: 'IT2403',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'F1',
+        subject: 'IT2401',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'PlayArea',
+        subject: 'P.T',
+        type: 'Playtime'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: 'F1',
+        subject: 'IT2404',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'F1',
+        subject: 'IT2405',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: 'F1',
+        subject: 'MA2401',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'F1',
+        subject: 'IT2401',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'F1',
+        subject: 'IT2405',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: 'F1',
+        subject: 'Aptitude',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'F1',
+        subject: 'IT2403',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'F1',
+        subject: 'IT2403',
+        type: 'Tutorial'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'F1',
+        subject: 'IT2405',
+        type: 'Tutorial'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'F10',
+        subject: 'MA2401',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'PlayArea',
+        subject: 'P.T',
+        type: 'PlayTime'
+    }
+]
